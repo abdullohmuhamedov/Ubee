40c8aba2e86d60df8821b78102dc58d10cd86b5b
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-05-17 16:02:00 +0500

    User Service is written

--- Ubee.Domain/Commons/Auditable.cs
+++ Ubee.Domain/Commons/Auditable.cs
@@ -6,6 +6,4 @@ public abstract class Auditable
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
     public bool IsDeleted { get; set; }
-    public int CreatedBy { get; set; }
-    public int? UpdatedBy { get; set; }
 }
---
+++ Ubee.Service/Helpers/PasswordHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Ubee.Service.Helpers;
+
+public class PasswordHelper
+{
+    public static string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+    public static bool Verify(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
